ceb0ea1a47948b1c0654c88b11e17f3c44b52862
Atevaz <101355785+Atevaz@users.noreply.github.com>
2022-07-17 08:00:37 +0200

    first commit

--- lib/presentation/styles/colors.dart
+++ lib/presentation/styles/colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColor{
   static const Color white = Color(0xFFFFFFFF);
+  static const Color pink = Color(0xFFF0446D);
+  static const Color lightGrey = Color(0xFFF6F6F6);
+  static const Color black = Color(0xFF000000);
 }
+
